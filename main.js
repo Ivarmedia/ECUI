4c6bb3f2d79f27c6bbdac09afd22f65d63325aa7
Alvaro <alvaro.rodriguez@elastacloud.com>
2023-12-07 10:49:34 +0100

    fuck you

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var ECUI;
 
 //import('./ECUI.js').then((ECUIModule) => {
-  import('https://ivarmedia.com/ECUI/ECUI.js').then((ECUIModule) => {
+  import('https://raw.githubusercontent.com/Ivarmedia/ECUI/main/ECUI.js').then((ECUIModule) => {
     var element = document.querySelector(".here");
     ECUI = new ECUIModule.ECUI(element, null);
   }).catch((error) => {
